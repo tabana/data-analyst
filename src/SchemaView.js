9d68994d6f11f640d2c51d67a978a07137d9cca5
Rangongo, Tim <Timothy.Rangongo@tdsecurities.com>
2017-05-03 11:24:19 -0400

    Removed DataGridView.

--- src/SchemaView.js
+++ src/SchemaView.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import SchemaStore from './SchemaStore';
 import SchemaActions from './SchemaActions';
-import DataGridView from './DataGridView';
 
 class SchemaView extends Component {
   constructor(props) {
